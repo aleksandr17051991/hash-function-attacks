da77e044e63c311c699e035b6a3f43ed105205cf
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2025-04-25 23:40:39 +0300

    Feat: created scammer's initial data

--- lenghtExceptionAttack.js
+++ lenghtExceptionAttack.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const crypto = require('crypto');
-const { get } = require('https');
 
-const hashMD5 = 'md5';
-const hashSHA1 = 'sha1';
+const hashMD5 = 'md5',
+  hashSHA1 = 'sha1';
 
-const secret = 'password';
+const secret = 'qwerty1234567890';
 const userMessage = 'user=Alex,amount=100';
 
 function getHash(data, hashAlgorithm) {
@@ -17,11 +16,23 @@ function getHash(data, hashAlgorithm) {
 const userSignature = getHash(secret + userMessage, hashSHA1);
 console.log('user signature >>>', userSignature);
 
-// 2) server recieved "message" and try to varify its user authenticity. Server know "secret"
+// 2) server recieved "message" and try to varify its user authenticity. Server knows "secret"
 const serverVarifiedHash = getHash(secret + userMessage, hashSHA1);
+console.log('Server varification hash >>>', serverVarifiedHash);
 // Server compare hashes
 console.log(
   serverVarifiedHash === userSignature
-    ? 'Correct user!'
-    : 'Error: verification failed'
+    ? '✅ Correct user!'
+    : '❌ Error: verification failed'
 );
+
+// Scammer action:
+
+// Want to send new message with additional info: knownOrigMsg + '&admin=true'
+// Knows: knownSignature and knownOrigMsg
+// Must guess: guessedSecretLength
+
+const knownOrigMsg = userMessage;
+const knownSignature = userSignature;
+const appendInfo = '&admin=true';
+let guessedSecretLength = 16;
